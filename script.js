b0576d17d882d59afe3cf6e4151cc8e08cb7d922
Tshakalisa Khupe <88752230+tkhupe@users.noreply.github.com>
2023-02-16 14:13:32 -0600

    completed

---
+++ script.js
@@ -0,0 +1,23 @@
+var one = 64;
+var two = "656302";
+var three = false;
+var four = 64.55;
+var five = "Howdy!";
+var six;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof one); //number or interger because 64 is a number//
+console.log(typeof two);  //string because of "" element//
+console.log(typeof three); //boolean because of false or true being //
+console.log(typeof four); //number//
+console.log(typeof five);// string//
+console.log(typeof six);// undifined because the variable has not yet been defined//
+
+four = "Hello!";
+five = false;
+six = 23;
+
+// Insert comments to explain what each console log below will log to the console
+console.log(typeof four); // 'Hello' because the variable has been reassigned//
+console.log(typeof five); // Boolean because thats the value//
+console.log(typeof six); // Number because 23 is a number//
